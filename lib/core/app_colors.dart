7ad3bbb8393a0da4a2c78aad488b37e535ddc028
Yasmin Wael <yasminwael2306@gmail.com>
2026-09-05 19:05:57 +0300

    onboarding

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class AppColors {
+  static final Color primaryColor = Color(0xFFF6BD00);
+  static final Color black = Color(0xFF121312);
+  static final Color red = Color(0xFFE82626);
+  static final Color white = Color(0xFFFFFFFF);
+}
